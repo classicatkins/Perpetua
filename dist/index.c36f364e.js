7e6413094f4dd50aaa9adc81810b80465c19338a
Melissa Atkins <classicatkins@gmail.com>
2023-12-22 16:38:48 -0600

    works

---
+++ dist/index.c36f364e.js
@@ -0,0 +1,6 @@
+// add menu toggle to bars icon in nav bar
+document.querySelector(".fa-bars").addEventListener("click", ()=>{
+    document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+});
+
+//# sourceMappingURL=index.c36f364e.js.map
