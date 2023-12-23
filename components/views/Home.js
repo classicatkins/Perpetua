841799e8a5ea754953d5ca32b305a6a01260d6f6
Melissa Atkins <classicatkins@gmail.com>
2023-12-23 15:06:51 -0600

    buttons

--- components/views/Home.js
+++ components/views/Home.js
@@ -3,6 +3,12 @@ export default state => html`
   <section id="jumbotron">
     <h2>SavvyCoders JavaScript Fullstack Bootcamp</h2>
     <a href="index.html">"Call to Action" "Button"</a>
+
+    <button id="createUserButton">Create User</button>
+    <button id="createGraphButton">Create Graph</button>
+    <button id="recordPixelButton">Record Pixel</button>
+    <button id="updatePixelButton">Update Pixel</button>
+    <button id="deletePixelButton">Delete Pixel</button>
   </section>
      <!-- <main>-->
 <!--        <section id="home">-->
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// import { Header, Nav, Main, Footer } from "./components";
+// Assuming these components are defined in your project
 import { Nav, Main, Footer } from "./components";
 import * as store from "./store";
 import Navigo from "navigo";
@@ -8,104 +8,90 @@ import axios from "axios";
 const router = new Navigo("/");
 
 function render(state = store.Home) {
-  document.querySelector("#root").innerHTML = `
-    ${Nav(store.Links)}
-    ${Main(state)}
-    ${Footer()}
-  `;
-  router.updatePageLinks();
-  afterRender();
+    document.querySelector("#root").innerHTML = `
+        ${Nav(store.Links)}
+        ${Main(state)}
+        ${Footer()}
+    `;
+    router.updatePageLinks();
+    afterRender();
 }
 
 function afterRender() {
-  // add menu toggle to bars icon in nav bar
-  document.querySelector(".fa-bars").addEventListener("click", () => {
-    document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+    document.querySelector(".fa-bars").addEventListener("click", () => {
+        document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+    });
+
+    setupPixelaEventListeners();
+}
+
+function setupPixelaEventListeners() {
+  const createUserButton = document.getElementById("createUserButton");
+  if (createUserButton) {
+      createUserButton.addEventListener("click", () => {
+          createUser("classicatkins", "token"); // Replace 'username' and 'token' with actual values
+      });
+  }
+
+  // Here you can add more event listeners for other Pixela API actions if needed
+  // For example:
+  // const createGraphButton = document.getElementById("createGraphButton");
+  // if (createGraphButton) {
+  //     createGraphButton.addEventListener("click", () => {
+  //         createGraph("username", "token", "graphId", ...); // Replace with actual values and implement createGraph function
+  //     });
+  // }
+}
+
+function createUser(username, token) {
+  console.log("Creating user:", username, token);
+  // Implement the Axios POST request to create a user
+  axios.post('https://pixe.la/v1/users', {
+      token: token,
+      username: username,
+      agreeTermsOfService: 'yes',
+      notMinor: 'yes'
+  })
+  .then(response => {
+      console.log('User creation response:', response.data);
+  })
+  .catch(error => {
+      console.error('Error creating user:', error);
   });
 }
 
 router.hooks({
-  before: (done, params) => {
-    const view =
-      params && params.data && params.data.view
-        ? capitalize(params.data.view)
-        : "Home";
+    before: (done, params) => {
+        const view = params && params.data && params.data.view ? capitalize(params.data.view) : "Home";
 
-    // Add a switch case statement to handle multiple routes
-    switch (view) {
-      case "Home":
-        axios
-          .get(
-            `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&q=st%20louis`
-          )
-          .then(response => {
-            const kelvinToFahrenheit = kelvinTemp =>
-              Math.round((kelvinTemp - 273.15) * (9 / 5) + 32);
-
-            store.Home.weather = {
-              city: response.data.name,
-              temp: kelvinToFahrenheit(response.data.main.temp),
-              feelsLike: kelvinToFahrenheit(response.data.main.feels_like),
-              description: response.data.weather[0].main
-            };
-            done();
-          })
-          .catch(err => {
-            console.log(err);
-            done();
-          });
-        break;
-      // Added in Lesson 7.1
-      case "Pizza":
-        axios
-          .get(`${process.env.PIZZA_PLACE_API_URL}/pizzas`)
-          .then(response => {
-            store.Pizza.pizzas = response.data;
-            done();
-          })
-          .catch(error => {
-            console.log("It puked", error);
-            done();
-          });
-        break;
-      default:
-        done();
+        switch (view) {
+            case "Home":
+                // Add any specific logic for the Home view
+                done();
+                break;
+            // Implement other cases as needed
+            default:
+                done();
+        }
+    },
+    already: params => {
+        const view = params && params.data && params.data.view ? capitalize(params.data.view) : "Home";
+        render(store[view]);
     }
-  },
-  already: params => {
-    const view =
-      params && params.data && params.data.view
-        ? capitalize(params.data.view)
-        : "Home";
-
-    render(store[view]);
-  }
 });
 
-router
-  .on({
+router.on({
     "/": () => render(),
     ":view": params => {
-      let view = capitalize(params.data.view);
-      if (view in store) {
-        render(store[view]);
-      } else {
-        render(store.Viewnotfound);
-        console.log(`View ${view} not defined`);
-      }
+        let view = capitalize(params.data.view);
+        if (view in store) {
+            render(store[view]);
+        } else {
+            render(store.Viewnotfound);
+            console.log(`View ${view} not defined`);
+        }
     }
-  })
-  .resolve();
+}).resolve();
 
-// render();
-
-// add menu toggle to bars icon in nav bar
-// document.querySelector(".fa-bars").addEventListener("click", () => {
-//   document.querySelector("nav > ul").classList.toggle("hidden--mobile");
-// });
-
- /* ${Header(state)} */
-
-axios.get(
-  "https://api.openweathermap.org/data/2.5/weather?q=St.%20Louis&APPID=723e0986e0f98b33c0d046e7f38d272c"
-);
+// Example Axios GET request
+axios.get("https://api.openweathermap.org/data/2.5/weather?q=St.%20Louis&APPID=723e0986e0f98b33c0d046e7f38d272c");
